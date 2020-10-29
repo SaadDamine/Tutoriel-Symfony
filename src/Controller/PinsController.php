6c672542ae8566ce53c2aef390ee617fa52c375e
SaadDamine <m.sdaminesaad@gmail.com>
2020-10-29 19:30:10 +0100

    create pins
     Formulaire de création de pins

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -7,6 +7,7 @@ use App\Repository\PinRepository;
 use Doctrine\ORM\EntityManagerInterface;
 //use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,20 +17,28 @@ class PinsController extends AbstractController
      * @Route("/")
      */
     public function index(PinRepository $repo): Response
-    {
-        //EntityManagerInterface $em
-        /*$pin = new Pin;
-        $pin->setTitle('Title 3');
-        $pin->setDescription('Description 3');
-
-        $em->persist($pin);
-        $em->flush();
-
-        //dd($pin);	*/
-        
-        //$repo = $em->getRepository(Pin::class);
+    {      
         $pins = $repo->findAll();
-
         return $this->render('pins/index.html.twig',['pins' => $pins]);
+    }
+
+    /**
+     * @Route("/pins/create",methods={"GET","POST"})
+     */
+    public function create(Request $request,EntityManagerInterface $em)
+    {
+        if($request->isMethod("POST")){
+            $data = $request->request->all();
+
+            $pin = new Pin;
+            $pin->setTitle($data['title']);
+            $pin->setDescription($data['description']);
+
+            $em->persist($pin);
+            $em->flush();
+
+            return $this->redirect('/');
+        }
+        return $this->render('pins/create.html.twig');
     }
 }
