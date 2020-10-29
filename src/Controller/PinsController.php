3d11b78c03c348fd3e605f9b3562f3ef4e4abe30
SaadDamine <m.sdaminesaad@gmail.com>
2020-10-29 17:18:24 +0100

    Update PinsController.php
     Injection de dépendances

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,16 +14,15 @@ class PinsController extends AbstractController
     /**
      * @Route("/")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
         $pin = new Pin;
-        $pin->setTitle('Title 1');
-        $pin->setDescription('Description 1');
-
-        $em = $this->getDoctrine()->getManager();
+        $pin->setTitle('Title 3');
+        $pin->setDescription('Description 3');
 
         $em->persist($pin);
         $em->flush();
+
     	//dd($pin);	
 
         return $this->render('pins/index.html.twig');
