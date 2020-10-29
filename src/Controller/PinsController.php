2a82b42d2f058dc8d1dbbde12cb26a58983ee040
SaadDamine <m.sdaminesaad@gmail.com>
2020-10-29 16:56:00 +0100

    Les packs Debug et Profiler
    composer require debug --dev
    composer require profiler --dev

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,11 @@ class PinsController extends AbstractController
      */
     public function index(): Response
     {
+    	$pin = new Pin;
+
+    	dump($pin);
+    	die;
+
         return $this->render('pins/index.html.twig');
     }
 }
